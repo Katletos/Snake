100c03144e04f148d24d42a713fd70fc491ad99e
melkit235 <verghel.iliya@gmail.com>
2021-10-19 00:21:29 +0300

    Переделал поле
    еще не все

--- src/field.java
+++ src/field.java
@@ -1,7 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
-public class field {
+public class field extends JPanel {
+
+   /* public class block {
+        public int posy;
+        public int posx;
+
+
+        public block(int x, int y) {
+            posx = x;
+            posy = y;
+        }
+
+        public void SetRandomPosition() {
+            posx = (int) (Math.random() * 20 - 1);
+            posy = (int) (Math.random() * 20 - 1);
+        }
+    }*/
+
+
+
+
+    public static JFrame jFrame;
+    public static final int scale =32;                 //размер клетки
+
+    //отрисовка клеток на поле
+    public void paint(Graphics g){
+        g.setColor(Color.black);
+        g.fillRect(0,0,1000,1000);
+        for (int x= 0; x<1000; x+=scale){
+            g.setColor(Color.white);
+            g.drawLine(x,0,x,1000);
+        }
+        for (int y=0; y<1000; y+=scale){
+            g.setColor(Color.white);
+            g.drawLine(0,y,1000,y);
+        }
+        //for (int i=0; i<20; i++){
+       //     g.setColor(Color.white);
+        //    g.fillRect(block.posx*scale+1, blok.posy*scale+1,scale-1,scale-1);
+        //}
+    }
+
+
     public static void main(String[] args)
     {
+
         Scanner in = new Scanner(System.in);
         System.out.println("Выберите размер поля");
         System.out.println("1:Большое; 2:Среднее; 3:Маленькое");
@@ -11,9 +56,10 @@ public class field {
         System.out.println("1:Нет; 2:Мало; 3:Средне; 4:Много");
         int colich = in.nextInt();
 
+
         if (raz==1)
         {
-            int razmer=1000;
+             raz=scale*20;
             switch (colich) {
                 case 1:
                     colich = 0;
@@ -33,7 +79,7 @@ public class field {
 
         if (raz==2)
         {
-            int razmer=500;
+             raz=scale*15;
             switch (colich) {
                 case 1:
                     colich = 0;
@@ -53,7 +99,7 @@ public class field {
 
         if (raz==3)
         {
-            int razmer=250;
+             raz=scale*10;
             switch (colich) {
                 case 1:
                     colich = 0;
@@ -70,11 +116,27 @@ public class field {
             }
 
         }
-        int[][] pol = new int[1000][1000];
-       for (int i = 1; i < 110; i++){
-           for (int j=1;j < 110;j++) {
 
-           }
-       }
+        int col=colich;
+        int razmerx=raz+17;
+        int razmery=raz+40;
+
+        //создание окна для змейки
+        jFrame = new JFrame("Snake");
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.setResizable(false);
+        jFrame.add(new field());
+        jFrame.setSize(razmerx,razmery);
+
+
+       // block= new block ((int) (Math.random() * 20 - 1),(int) (Math.random() * 20 - 1));
+
+        int x;
+        int y;
+       // for (int i=0; i<colich; i++){
+
+       // }
     }
     }
